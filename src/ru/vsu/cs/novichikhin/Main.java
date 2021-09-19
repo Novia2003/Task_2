4ad135d5823628da2d4ded60980e15796b463ede
Novia2003 <90568044+Novia2003@users.noreply.github.com>
2021-09-19 22:12:17 +0300

    Add

---
+++ src/ru/vsu/cs/novichikhin/Main.java
@@ -0,0 +1,41 @@
+package ru.vsu.cs.novichikhin;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n = readValue("количество мячей забитых первой командой: N = ");
+        chekValue(n);
+        int m = readValue("количество мячей забитых второй командой: M = ");
+        chekValue(m);
+        compareValue(n, m);
+
+    }
+
+    static int readValue(String value) {
+        System.out.print("Введите " + value);
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
+    }
+
+    static void chekValue(int val) {
+        if (val < 0) {
+            System.out.print("Данные введены неверно.");
+            System.exit(0);
+        }
+    }
+
+    static void compareValue(int a, int b) {
+        if (a == b) {
+            System.out.print("Обе команды заработали по 1 очку");
+        } else {
+            if (a > b) {
+                System.out.print("Первая команда заработала 3 очка, а вторая команда заработала 0 очков");
+            } else {
+                System.out.print("Первая команда заработала 0 очков, а вторая команда заработала 3 очка");
+            }
+        }
+    }
+}
